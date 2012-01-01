1707399ff056831955120461710797160846f32d
Andy Brett <andy@andybrett.com>
2012-01-01 08:20:26 -0800

    first example in readme now works

--- lib/session.rb
+++ lib/session.rb
@@ -1,9 +1,15 @@
+require 'httparty'
+
 module Jawbone
   class Session
     attr_accessor :token
     
+    include HTTParty
+    
     def initialize email, password
-      
+      json = self.class.post "https://jawbone.com/user/signin/login", :query => 
+        { :service => "nudge", :email => email, :pwd => password }
+      @token = json["token"]
     end
   end
 end
